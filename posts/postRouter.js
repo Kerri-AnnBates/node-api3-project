8a7fc6945c06c12daaad5a88598b989db7a74ae0
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-04 15:45:35 -0500

    set up GET, PUT, DELETE requests for posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,70 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('./postDb');
 
 router.get('/', (req, res) => {
-  // do your magic!
+
+  db.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({ messege: 'Unable to retrieve posts' });
+    })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  db.getById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Unable to find post by that ID' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ messege: 'Unable to retrieve post' });
+    })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(200).json(deleted);
+      } else {
+        res.status(404).json({ message: 'Unable to find post by that ID' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Unable to delete post' });
+    })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const newPost = req.body;
+  const id = req.params.id;
+
+  db.getById(id).then(post => {
+    if (!post) {
+      res.status(404).json({ message: 'Unable to find post by that ID' });
+    }
+  })
+  if(newPost.text) {
+    db.update(id, newPost)
+      .then(updated => {
+        res.status(200).json(updated);
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Unable to delete post' });
+      })
+  } else {
+    res.status(400).json({ errorMessage: "Please provide text for the post." });
+  }
 });
 
 // custom middleware
